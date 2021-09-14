f5f850a8a71abc929d5726ed31e804e371173cd2
sravn25 <victor.sravn24@gmail.com>
2021-09-15 00:36:25 +0800

    added word frequency

--- words.py
+++ words.py
@@ -45,12 +45,21 @@ def search(s):
         print("time")
 
 
-def freq(s):
+def charFreq(s):
+    s = s.replace(" ", "")
     x = {char: s.count(char) for char in s}
     x = dict(sorted(x.items(), key=lambda item: item[1], reverse=True))
     for letter in x:
-        if letter != " ": 
-            print(letter, ":", x[letter])
+        print(letter, ":", x[letter])
+    return [(next(iter(x.keys()))), (next(iter(x.values())))]
+
+def wordFreq(s):
+    s = s.split()
+    w = {word: s.count(word) for word in s}        
+    w = dict(sorted(w.items(), key=lambda item: item[1], reverse=True))
+    for word in w:
+        print(word, ":", w[word])
+    return [(next(iter(w.keys()))), (next(iter(w.values())))]
 
 
 def menu(s):
@@ -60,9 +69,10 @@ def menu(s):
         print(s, end="\n\n")
         print("1. Word and Character count")
         print("2. Occurence of word")
-        print("3. Frequency of Words")
-        print("4. Change sentence or paragraph")
-        print("5. Quit")
+        print("3. Frequency of Characters")
+        print("4. Frequency of Words")
+        print("5. Change sentence or paragraph")
+        print("6. Quit")
 
         opt = input("\nWhat would you like to do?\n")
 
@@ -73,12 +83,15 @@ def menu(s):
             search(s)
 
         elif opt == "3":
-            freq(s)
+            charFreq(s)
 
         elif opt == "4":
-            s = entry()
+            wordFreq(s)
 
         elif opt == "5":
+            s = entry()
+
+        elif opt == "6":
             input("Goodbye")
             clear()
             raise SystemExit(0)
